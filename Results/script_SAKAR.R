b8c3bde58e91fb9bf4747abc1fca79105820ce18
Jula McGibbon <julamcgibbon@gmail.com>
2017-06-03 19:23:35 +0200

    Adding sampling options script for CART and SAKAR

---
+++ Results/script_SAKAR.R
@@ -0,0 +1,8 @@
+numberOfRounds <- 5 #number of times experiment should be repeated
+minImprovementPerRound <- 0.0001 #complexity step
+#progression base #(default = 1)
+#sampling range lower value #(default = 1)
+#sampling range upper value #(default = 1)
+#sampling progression #(default = 1)
+#complexLower #complexity range lower value #default = 0
+#complexUpper #complexity range upper value # default = 0.001
